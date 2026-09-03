39c3e667986e47c66ca5446bb9d86b27425119c7
BoltyPlays <113870837+BoltyPlays@users.noreply.github.com>
2026-09-03 00:34:14 -0400

    Made one feature (block input)

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import tkinter as tk
+import keyboard
 
+def lockKeyboard():
+    for keys in range(200):
+        keyboard.block_key(keys)
+        button.configure(text = "Unblock input", command=unlockKeyboard)
+
+def unlockKeyboard():
+    for keys in range(200):
+        keyboard.unblock_key(keys)
+        button.configure(text = "Halt input", command=lockKeyboard)
+
+root=tk.Tk()
+label =tk.Label(root, text="Halt! Input")
+label.pack()
+
+button = tk.Button(root, text="Halt Input", width=25, command=lockKeyboard)
+button.pack()
+
+root.mainloop()
